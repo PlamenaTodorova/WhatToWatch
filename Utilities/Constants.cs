a79b6bfc365ab6eb76e7a09d71e89528ef6d601d
Plamena Todorova <plamena.i.todorova@gmail.com>
2018-10-27 19:25:56 +0200

    Update Constants.cs

--- Utilities/Constants.cs
+++ Utilities/Constants.cs
@@ -6,6 +6,7 @@
         public static string GetSeries = "https://api.thetvdb.com/series/{0}";
         public static string GetEpisode = "https://api.thetvdb.com/series/{0}/episodes/query?airedSeason={1}&airedEpisode={2}";
         public static string GetSearch = "https://api.thetvdb.com/search/series?slug={0}";
+        public static string GetPoster = "https://api.thetvdb.com/series/{0}/images/query?keyType=season&subKey={1}";
 
         //Files paths
         public static string Folder = @"\Files";
