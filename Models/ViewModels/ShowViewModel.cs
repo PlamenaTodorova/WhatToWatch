8f09c9cbaa0a3bed3c281aabe0d7269876c8a593
Plamena Todorova <plamena.i.todorova@gmail.com>
2018-10-28 00:48:17 +0200

    Update ShowViewModel.cs

--- Models/ViewModels/ShowViewModel.cs
+++ Models/ViewModels/ShowViewModel.cs
@@ -1,111 +1,107 @@
 using System.Collections.Generic;
+using WhatToWatch.Models.JsonModels;
+using WhatToWatch.Utilities;
+using System;
 
 namespace WhatToWatch.Models.ViewModels
 {
-    using global::WhatToWatch.Models.JsonModels;
-    using global::WhatToWatch.Utilities;
-    using System;
-
-    namespace WhatToWatch.Models.ViewModels
+    public class ShowViewModel : IComparable<ShowViewModel>
     {
-        public class ShowViewModel : IComparable<ShowViewModel>
+        public ShowViewModel(Show show)
         {
-            public ShowViewModel(Show show)
+            this.Id = show.Id;
+            this.Title = show.Title;
+            this.CurrentEpisode = show.CurrentEpisode;
+            this.CurrentSeason = show.CurrentSeason;
+            this.Status = "None";
+            this.RealStatus = show.Status.ToString();
+
+            this.PosterSource = HelperFunctions.GetPosterSorce(show.Id, show.CurrentSeason);
+
+        }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int CurrentSeason { get; set; }
+
+        public int CurrentEpisode { get; set; }
+
+        public string PosterSource { get; set; }
+
+        public string Status { get; set; }
+
+        public string EpisodeTitle { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+
+        private string RealStatus { get; set; }
+
+        public void UpdateEpisodeInfo(List<EpisodeInfoJson> data)
+        {
+            if (data != null && data.Count != 0)
             {
-                this.Id = show.Id;
-                this.Title = show.Title;
-                this.CurrentEpisode = show.CurrentEpisode;
-                this.CurrentSeason = show.CurrentSeason;
-                this.Status = "None";
-                this.RealStatus = show.Status.ToString();
-
-                this.PosterSource = HelperFunctions.GetPosterSorce(show.Id, show.CurrentSeason);
-
+                EpisodeTitle = data[0].episodeName;
+                if (data[0].firstAired != "" && data[0].firstAired != null)
+                    ReleaseDate = DateTime.Parse(data[0].firstAired);
+                else ReleaseDate = null;
             }
 
-            public int Id { get; set; }
+            if (IsDue())
+                this.Status = this.RealStatus;
+            else
+                this.Status = "None";
+        }
 
-            public string Title { get; set; }
+        public void UpdateEpisodeInfo()
+        {
+            this.EpisodeTitle = "";
+            this.ReleaseDate = null;
+            this.Status = "None";
+        }
 
-            public int CurrentSeason { get; set; }
+        public int CompareTo(ShowViewModel other)
+        {
+            if (this.ReleaseDate == null)
+                return 1;
 
-            public int CurrentEpisode { get; set; }
+            if (other.ReleaseDate == null)
+                return -1;
 
-            public string PosterSource { get; set; }
-
-            public string Status { get; set; }
-
-            public string EpisodeTitle { get; set; }
-
-            public DateTime? ReleaseDate { get; set; }
-
-            private string RealStatus { get; set; }
-
-            public void UpdateEpisodeInfo(List<EpisodeInfoJson> data)
+            if (this.IsDue() && other.IsDue())
             {
-                if (data != null && data.Count != 0)
+                if (this.Status == other.Status)
                 {
-                    EpisodeTitle = data[0].episodeName;
-                    if (data[0].firstAired != "" && data[0].firstAired != null)
-                        ReleaseDate = DateTime.Parse(data[0].firstAired);
-                    else ReleaseDate = null;
+                    if (this.ReleaseDate > other.ReleaseDate)
+                        return 1;
+                    return -1;
                 }
 
-                if (IsDue())
-                    this.Status = this.RealStatus;
-                else
-                    this.Status = "None";
-            }
-
-            public void UpdateEpisodeInfo()
-            {
-                this.EpisodeTitle = "";
-                this.ReleaseDate = null;
-                this.Status = "None";
-            }
-
-            public int CompareTo(ShowViewModel other)
-            {
-                if (this.ReleaseDate == null)
-                    return 1;
-
-                if (other.ReleaseDate == null)
+                if (this.Status == "Red")
                     return -1;
 
-                if (this.IsDue() && other.IsDue())
-                {
-                    if (this.Status == other.Status)
-                    {
-                        if (this.ReleaseDate > other.ReleaseDate)
-                            return 1;
-                        return -1;
-                    }
-
-                    if (this.Status == "Red")
-                        return -1;
-
-                    if (other.Status == "Red")
-                        return 1;
-
-                    if (this.Status == "Yellow")
-                        return 1;
-
-                    if (other.Status == "Yellow")
-                        return -1;
-                }
-
-                if (this.ReleaseDate > other.ReleaseDate)
+                if (other.Status == "Red")
                     return 1;
 
-                return -1;
+                if (this.Status == "Yellow")
+                    return 1;
+
+                if (other.Status == "Yellow")
+                    return -1;
             }
 
-            private bool IsDue()
-            {
-                if (ReleaseDate.HasValue)
-                    return ReleaseDate.Value.Date < DateTime.Now.Date;
-                return false;
-            }
+            if (this.ReleaseDate > other.ReleaseDate)
+                return 1;
+
+            return -1;
+        }
+
+        private bool IsDue()
+        {
+            if (ReleaseDate.HasValue)
+                return ReleaseDate.Value.Date < DateTime.Now.Date;
+            return false;
         }
     }
 }
