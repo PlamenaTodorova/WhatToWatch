a4beb971d1f48f70608840bb94f28ff680bc25dd
Plamena Todorova <plamena.i.todorova@gmail.com>
2018-10-28 00:48:52 +0200

    Update FollowedController.cs

--- Controllers/FollowedController.cs
+++ Controllers/FollowedController.cs
@@ -9,7 +9,7 @@ using System.Threading;
 using WhatToWatch.Models;
 using WhatToWatch.Models.BindingModels;
 using WhatToWatch.Models.JsonModels;
-using WhatToWatch.Models.ViewModels.WhatToWatch.Models.ViewModels;
+using WhatToWatch.Models.ViewModels;
 using WhatToWatch.Parsers;
 using WhatToWatch.Utilities;
 
