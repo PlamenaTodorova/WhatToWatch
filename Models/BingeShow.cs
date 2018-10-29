001e11226416aba1210f29b81409db3f1528d54d
Plamena Todorova <plamena.i.todorova@gmail.com>
2018-10-29 12:50:33 +0100

    Add files via upload

---
+++ Models/BingeShow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatToWatch.Models.BindingModels;
+
+namespace WhatToWatch.Models
+{
+    public class BingeShow
+    {
+        //Properties
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int CurrentSeason { get; set; }
+
+        public int CurrentEpisode { get; set; }
+
+        public void UpdateData(ShowBindingModel model)
+        {
+            this.Title = model.Name;
+            this.CurrentEpisode = model.CurrentEpisode;
+            this.CurrentSeason = model.CurrentSeason;
+        }
+    }
+}
