6b490e48ba7c4dc77fd51ba67a6aea07a3b4d8df
Plamena Todorova <plamena.i.todorova@gmail.com>
2018-10-29 12:50:08 +0100

    Add files via upload

---
+++ Models/JsonModels/SeasonsInfoJson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatToWatch.Models.JsonModels
+{
+    public class SeasonsDataJson
+    {
+        public List<string> airedSeasons { get; set; }
+        public string airedEpisodes { get; set; }
+    }
+
+    public class SeasonDataRoot
+    {
+        public SeasonsDataJson data { get; set; }
+    }
+}
